9bef34f736ac700a68ed829426a5155c91d87fd3
yu_mingzheng <yu_mingzheng@qq.com>
2022-09-22 14:06:09 +0800

    test

--- 数据爬虫/Crawler.py
+++ 数据爬虫/Crawler.py
@@ -6,3 +6,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+base_url = "https://www.imdb.com/title/tt7362036/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"
+}
